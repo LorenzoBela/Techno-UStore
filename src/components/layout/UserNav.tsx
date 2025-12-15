67d5cad4e8adf565179072d25ed86a7be0c49363
LorenzoBela <lorenzo.miguel.bela429@adamson.edu.ph>
2025-12-15 13:30:45 +0800

    Logout Fix

--- src/components/layout/UserNav.tsx
+++ src/components/layout/UserNav.tsx
@@ -118,7 +118,7 @@ export function UserNav() {
                     )}
                 </DropdownMenuGroup>
                 <DropdownMenuSeparator />
-                <DropdownMenuItem onClick={handleSignOut}>
+                <DropdownMenuItem onSelect={handleSignOut}>
                     <LogOut className="mr-2 h-4 w-4" />
                     <span>Log out</span>
                 </DropdownMenuItem>
